c44c0795abad9e330f43bf43b97095b163e6d2b1
Nimira43 <n.rabone72@aol.com>
2024-12-13 00:50:43 +0000

    Created renderPage to handle rendering a reusable function for rendering templates

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,6 +1,7 @@
 package handlers
 
 import (
+	"log"
 	"net/http"
 
 	"github.com/CloudyKit/jet/v6"
@@ -12,5 +13,22 @@ var views = jet.NewSet(
 )
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	
+	err := renderPage(w, "home.jet", nil)
+	if err != nil {
+		log.Println(err)
+	}
+}
+
+func renderPage(w http.ResponseWriter, tmpl string, data jet.VarMap) error {
+	view, err := views.GetTemplate(tmpl)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	err = view.Execute(w, data, nil)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+	return nil 
 }
