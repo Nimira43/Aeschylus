bb87075b8700b019e8c3674bb2272cea8bbab0dd
Nimira43 <n.rabone72@aol.com>
2025-08-03 14:20:01 +0100

    Project renamed to Aeschylus

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 function App() {
   return (
-    <div className='temp-container'>
-      <h1 className='text-grey-light bg-dark logo'>Forum Vitae</h1>
+    <div className='container'>
+      <div className='row'>
+        <div className='col'>
+          <h1 className='text-grey-light bg-dark logo'>Aeschylus</h1>
+        </div>
+      </div>
+      
       <p>Coming Soon</p>
     </div>
   )
