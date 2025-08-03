1d323d9493e8c142b49c6e01b12d190e86d8832f
Nimira43 <n.rabone72@aol.com>
2025-08-03 14:08:19 +0100

    Bringing in custom colour classes.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className='temp-container'>
-      <h1 className='logo'>Forum Vitae</h1>
+      <h1 className='text-grey-light bg-dark logo'>Forum Vitae</h1>
       <p>Coming Soon</p>
     </div>
   )
