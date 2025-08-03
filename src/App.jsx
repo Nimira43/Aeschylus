d972d40c2a77468eeb1c0d1bea8111fd4325fc52
Nimira43 <n.rabone72@aol.com>
2025-08-03 15:10:47 +0100

    Styling nav links

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,40 @@ function App() {
   return (
     <div className='container'>
       <div className='row'>
-        <div className='col mt-3'>
-          <h1 className='text-prime logo'>Aeschylus</h1>
+        <div className='col mt-4'>
+          <h2 className='text-prime logo'>Aeschylus</h2>
         </div>
-        <div className='col text-end mt-3'>
-          <a href='#' className='bg-prime text-light button'>Login
+        <div className='col text-end mt-4'>
+          <a 
+            href='#' 
+            className='bg-prime text-light button'
+          >
+            Login
           </a>
+        </div>
+        <hr className='mb-3'></hr>
+      </div>
+      <div className='row'>
+        <div className='col-md-2'>
+          <nav>
+            <div className='list-group'>
+              <a 
+                href='#'
+                className='list-group-item list-group-item-action'
+              >
+                Home
+              </a>
+              <a 
+                href='#'
+                className='list-group-item list-group-item-action'
+              >
+                Movies
+              </a>
+            </div>
+          </nav>
+        </div>
+        <div className='col-md-10'>
+
         </div>
       </div>
     </div>
