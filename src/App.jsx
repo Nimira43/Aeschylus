a47ddee6405688748a75095f66eb7f9a7871d6b9
Nimira43 <n.rabone72@aol.com>
2025-08-03 15:00:41 +0100

    Added mt-3

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,10 @@ function App() {
   return (
     <div className='container'>
       <div className='row'>
-        <div className='col'>
+        <div className='col mt-3'>
           <h1 className='text-prime logo'>Aeschylus</h1>
         </div>
-        <div className='col text-end'>
+        <div className='col text-end mt-3'>
           <a href='#' className='bg-prime text-light button'>Login
           </a>
         </div>
