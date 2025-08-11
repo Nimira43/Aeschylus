8068cd80290c8e5ecb86e7d5a0a043d4e0d1fa21
Nimira43 <n.rabone72@aol.com>
2025-08-11 02:41:32 +0100

    Corrected router setup.

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,12 @@ import './index.css'
 import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
-const router = createBrowserRouter({
+const router = createBrowserRouter([
+  {
   path: '/',
   element: <App />
-})
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
