4bbbfd7ed4f13cab55285d1c31010e7dcc84d32f
Nimira43 <n.rabone72@aol.com>
2025-08-11 02:50:21 +0100

    Markup for ErrorPage done

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,9 +1,19 @@
 import { useRouteError } from 'react-router-dom'
 
 export default function ErrorPage() {
-  const error = useRouteError
+  const error = useRouteError()
  
   return (
-    <div>ErrorPage</div>
+    <div className='container'>
+      <div className='row'>
+        <div className='col-md-6 offset-md-3'>
+          <h1 className='mt-3 logo text-main'>Page Not Found</h1>
+          <p>We cannot find the page you are looking for.</p>
+          <p>
+            <em>{error.statusText || error.message}</em>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
