b6941a14e9b2ef9121b23dea3779da833b00c931
Nimira43 <n.rabone72@aol.com>
2025-08-22 21:56:12 +0100

    Created movies state variable

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -1,4 +1,8 @@
+import { useState } from 'react'
+
 const Movies = () => {
+  const [movies, setMovies] = useState([]) 
+
   return (
     <div className='text-center'>
       <h2 className='text-prime logo'>Movies</h2> 
