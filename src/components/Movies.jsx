da9cc380eebb27ef3138abc614354f1933b38a5f
Nimira43 <n.rabone72@aol.com>
2025-08-22 22:07:11 +0100

    moviesList array -2

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -5,7 +5,14 @@ const Movies = () => {
 
   useEffect(() => {
     let moviesList = [
-      {}
+      {
+         id: 1,
+         title: 'Star Wars',
+         releaseDate: '1977-05-25',
+         runtime: 121,
+         mpaa_rating: 'PG',
+         description: 'The Rebel Alliance aims to destroy the tyrannical Empire\'s ultimate weapon, the Death Star. When the rebel leader Princess Leia is captured by the Empire, Luke Skywalker acquires stolen architectural plans for the Death Star and sets out to rescue her while learning the ways of the Force from the Jedi Master Obi-Wan Kenobi.'
+      },
     ]
   })
 
