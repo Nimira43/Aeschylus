cd25abb140ab4b1363ad4ef102a223029b7fa401
Nimira43 <n.rabone72@aol.com>
2025-09-05 08:08:16 +0100

    Temporarily copying over first movie object

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -6,7 +6,14 @@ const Movie = () => {
   let { id } = useParams()
 
   useEffect(() => {
-
+    let myMovie = {
+      id: 1,
+      title: 'Star Wars',
+      releaseDate: '1977-05-25',
+      runtime: 121,
+      mpaa_rating: 'PG',
+      description: 'The Rebel Alliance aims to destroy the tyrannical Empire\'s ultimate weapon, the Death Star. When the rebel leader Princess Leia is captured by the Empire, Luke Skywalker acquires stolen architectural plans for the Death Star and sets out to rescue her while learning the ways of the Force from the Jedi Master Obi-Wan Kenobi.'
+    }
   }, [id])
 
   return (
