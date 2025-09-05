cadb99f9d09fc0145286c12ce13d008fc908d205
Nimira43 <n.rabone72@aol.com>
2025-09-05 08:04:36 +0100

    getting id with useParams

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 const Movie = () => {
-  const [movie, setMovie] = useState({})  
+  const [movie, setMovie] = useState({}) 
+  let { id } = useParams()
+
   return (
     <div className='text-center'>
       <h2 className='text-prime logo'>Single Movie</h2> 
