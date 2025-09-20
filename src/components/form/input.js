c690353d25272e513534f873ba20bde24672d2a2
Nimira43 <n.rabone72@aol.com>
2025-09-21 00:39:22 +0100

    Created input element with various properties

--- src/components/form/input.js
+++ src/components/form/input.js
@@ -9,6 +9,17 @@ const Input = forwardRef((props, ref) => {
       >
         {props.title}
       </label>
+      <input 
+        type={props.type}
+        className={props.className}
+        id={props.name}
+        ref={ref}
+        name={props.name}
+        placeholder={props.placeholder}
+        onChange={props.onChange}
+        autoComplete={props.autoComplete}
+        value={props.value}
+      />
     </div>
   )
 })
