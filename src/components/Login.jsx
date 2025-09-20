d408770cd1f380b45a5e762132937d0908b09c21
Nimira43 <n.rabone72@aol.com>
2025-09-21 00:53:05 +0100

    Created password state variable

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 
 const Login = () => {
-  const [email, setEmail] = useState
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
 
   return (
     <div className='text-center'>
