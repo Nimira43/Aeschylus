f6506fb037672796ba7d3c7c6671680c0a0c1da7
Nimira43 <n.rabone72@aol.com>
2025-09-21 00:44:21 +0100

    Created email state variable in Login.tsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,8 @@
+import { useState } from 'react'
+
 const Login = () => {
+  const [email, setEmail] = useState
+
   return (
     <div className='text-center'>
       <h2 className='text-prime logo'>Login</h2> 
