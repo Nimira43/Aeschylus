9f573487325c99f9bd40662bb8aa5f9ad27c5b9b
Nimira43 <n.rabone72@aol.com>
2025-09-21 00:35:49 +0100

    Setting up label

--- src/components/form/input.js
+++ src/components/form/input.js
@@ -1,5 +1,14 @@
 import { forwardRef } from 'react'
 
 const Input = forwardRef((props, ref) => {
-  
+  return (
+    <div claddName='mb-3'>
+      <label 
+        htmFor={props.name}
+        className='form-label'
+      >
+        {props.title}
+      </label>
+    </div>
+  )
 })
