ae1c380d4f523ad3d7909d4578c28c697ecb391c
Nimira43 <n.rabone72@aol.com>
2025-10-09 23:35:26 +0100

    err - setting web server

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+)
+
 const port = 8080
 
 type application struct {
-
 }
 
 func main() {
 	var app application
 
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 }
