eecdf534cf6f608e4f222bd90d164d28db2dd967
Nimira43 <n.rabone72@aol.com>
2025-10-24 10:44:07 +0100

    Corrected lines 11 to 13

--- backend/cmd/api/handlers.go
+++ backend/cmd/api/handlers.go
@@ -8,9 +8,9 @@ import (
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 	var payload = struct {
-		Status string `json:"status"`,
-		Message string `json:"message"`,
-		Version string `json:"version"`,
+		Status string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
 	}{
 		Status: "active",
 		Message: "Aeschylus up and running",
