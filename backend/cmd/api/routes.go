9e0b68b8876b47b18e26105d7dbbae31c1048731
Nimira43 <n.rabone72@aol.com>
2025-10-24 10:11:34 +0100

    Middleware

--- backend/cmd/api/routes.go
+++ backend/cmd/api/routes.go
@@ -4,8 +4,13 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func (app *application) routes() http.Handler {
-	mux := chi.NewRouter
+	mux := chi.NewRouter()
+
+	mux.Use(middleware.Recoverer)
+
+	return mux
 }
