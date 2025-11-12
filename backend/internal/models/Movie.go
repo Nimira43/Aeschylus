d85a249c83007f28e4c4edad1ccd88b52e80b52c
Nimira43 <n.rabone72@aol.com>
2025-11-12 04:16:46 +0000

    Movie type - 3

--- backend/internal/models/Movie.go
+++ backend/internal/models/Movie.go
@@ -4,6 +4,9 @@ import "time"
 
 type Movie struct {
 	ID          int    `json:"id"`
-	Title       string `json:"title`
-	ReleaseDate time.Time `json:"release_date`
+	Title       string `json:"title"`
+	ReleaseDate time.Time `json:"release_date"`
+	RunTime	int `json:"runtime"`
+	MPAARating string `json:"mpaa_rating"`
+	Description string `json:"description"`
 }
