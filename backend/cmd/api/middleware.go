9142104e6bacae828c6f7511ebf97791158dd55c
Nimira43 <n.rabone72@aol.com>
2025-11-30 23:44:45 +0000

    middleware - 2

--- backend/cmd/api/middleware.go
+++ backend/cmd/api/middleware.go
@@ -4,6 +4,6 @@ import "net/http"
 
 func (app *application) enableCORS(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		
+		w.Header().Set("Access-Control-Allow-Origin", "http://*")
 	})
 }
