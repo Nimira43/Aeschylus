0fc5586fb603249a4317b88ceb3b396722c7a962
Nimira43 <n.rabone72@aol.com>
2025-11-30 23:40:36 +0000

    middleware - 1

---
+++ backend/cmd/api/middleware.go
@@ -0,0 +1,9 @@
+package main
+
+import "net/http"
+
+func (app *application) enableCORS(h http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		
+	})
+}
