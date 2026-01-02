97628f0e778f7a276e62a48e98d41a6f924623b0
Nimira43 <n.rabone72@aol.com>
2026-01-02 16:50:24 +0000

    Testing that the container has correctly got all tables.

--- frontend/src/components/Movie.jsx
+++ frontend/src/components/Movie.jsx
@@ -12,7 +12,7 @@ const Movie = () => {
       releaseDate: '1977-05-25',
       runtime: 121,
       mpaa_rating: 'PG',
-      description: 'The Rebel Alliance aims to destroy the tyrannical Empire\'s ultimate weapon, the Death Star. When the rebel leader Princess Leia is captured by the Empire, Luke Skywalker acquires stolen architectural plans for the Death Star and sets out to rescue her while learning the ways of the Force from the Jedi Master Obi-Wan Kenobi.'
+      description: 'Description.'
     }
     setMovie(myMovie)
   }, [id])
